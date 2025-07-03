878edee508590bcedf17a9b5bf0fec27c76b1a76
Michal Mordarski <mmordarski4@gmail.com>
2025-07-03 18:05:36 +0200

    Test function palindrom_check

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ def palindorm_check(word):
         if word[i] != word[(len(word)-1)-i]:
             return False
     return True
+
+print(palindorm_check("kajak"))
+print(palindorm_check("statek"))
