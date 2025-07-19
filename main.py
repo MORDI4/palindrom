9c0325717cbdcd001b760add8ee8be799e734eff
Michal Mordarski <mmordarski4@gmail.com>
2025-07-19 19:35:31 +0200

    Optimize palindrom_check function

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-def palindorm_check(word):
-    w = ''
-    for i in word:
-        if i.isalnum():
-            w += i.lower()
-    print(w)
-    for i in range(round(len(w)/2)):
-        if w[i] != w[(len(w)-1)-i]:
+def palindrom_check(word):
+    cleaned = [i.lower() for i in word if i.isalnum()]
+    for i in range(round(len(cleaned)/2)):
+        if cleaned[i] != cleaned[(len(cleaned)-1)-i]:
             return False   
     return True
 
-print(palindorm_check("kajak"))
-print(palindorm_check("Kobyla ma maly bok"))
-print(palindorm_check("statek"))
+print(palindrom_check("kajak"))
+print(palindrom_check("Kobyla ma maly bok"))
+print(palindrom_check("statek"))
